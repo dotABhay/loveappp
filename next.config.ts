95337e4ec6251f90e25a14cffa5d3251365e9960
Abhay Singhal <abhaysinghal126@gmail.com>
2024-12-10 01:55:43 +0545

    Your commit message

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+module.exports = {
+  
+  reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+  
+}

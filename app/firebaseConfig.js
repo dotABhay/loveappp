6b67a2edeaefd2f0e8cbcf885d9f30da9c8929a1
Abhay Singhal <abhaysinghal126@gmail.com>
2024-12-10 10:49:22 +0545

    Your commit message

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -1,28 +1,24 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"; // Import Firebase Authentication
-import { getFirestore } from "firebase/firestore"; // Import Firestore
 
-// Your web app's Firebase configuration from environment variables
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+let app: any;
 
-// Initialize Firebase Authentication
-const auth = getAuth(app);
+if (typeof window !== "undefined") {
+  app = initializeApp(firebaseConfig);
+}
 
-// Initialize Firestore
-const db = getFirestore(app);
-
-
+const auth = app ? getAuth(app) : null;
+const db = app ? getFirestore(app) : null;
 
 export { app, auth, db };
